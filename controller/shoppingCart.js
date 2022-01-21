b2f36947bff73ef25590be4a7e70ad915edef6ba
chhaya bagwan <76725368+chhaya742@users.noreply.github.com>
2022-01-21 15:02:59 +0530

    Update shoppingCart.js

--- controller/shoppingCart.js
+++ controller/shoppingCart.js
@@ -1,9 +1,6 @@
 const knex=require("../database/db")
 const generateUniqueId = require('generate-unique-id');
-const { put } = require("../routes/router");
-const { updatec } = require("./customer.con");
-const { DATETIME } = require("mysql/lib/protocol/constants/types");
- 
+
 
 generateId=(req,res)=>{
     const id = generateUniqueId({
@@ -19,13 +16,30 @@ addShopping_cart=(req,res)=>{
         cart_id :req.body.cart_id,
         product_id:req.body.product_id,
         attributes:req.body.attributes,
-        quantity :req.body.quantity,
+        quantity :1,
         added_on:new Date()
     }
     knex("shopping_cart").insert(add).where("shopping_cart.product_id",req.body.product_id)
     .then((d)=>{
-        res.send(d)
+        knex.select("shopping_cart.item_id","product.name",
+        "shopping_cart.attributes",
+        "product.product_id",
+        "product.price",
+        "shopping_cart.quantity",
+        "product.image").from("shopping_cart").join("product","shopping_cart.product_id","product.product_id").where("item_id",d)
+        .then((d2)=>{
+            for(i of d2){
+                const subtotal=d2[0].quantity*d2[0].price
+               i["subtotal"]=subtotal
+
+            }
+            
+            res.send(d2)
         })
+        .catch((e)=>{
+            res.send(e)
+        })
+})
     .catch((err)=>{
         res.send(err)
     })
@@ -40,25 +54,31 @@ getp=(req,res)=>{
 
 }
 // put
-Update=(req,res)=>{
-    knex("shopping_cart").where("item_id",req.params.id)
+Update1=(req,res)=>{
+    knex("shopping_cart")
+    .where("item_id",req.params.id)
     .update({
-        item_id:req.body.item_id,
-        // name: req.body.name,
-        attributes:req.body.attributes,
-        product_id:req.body.product_id,
-        // price:req.body.price,
-        quantity:req.body.quantity,
-        subtotal:req.body.subtotal
+        quantity:req.body.quantity
     })
     .then((data)=>{
         res.send(data)
+        knex.select("shopping_cart.item_id","product.name","shopping_cart.attributes","product.product_id","product.price","shopping_cart.quantity").from("shopping_cart")
+        .join("product","shopping_cart.product_id","product.product_id").where("cart_id",data)
+        .then((d)=>{
+            res.send(d)
+            console.log(d);
+        }).catch((e)=>{
+            res.send(e)
+            console.log(e);
+        })
+        
     }).catch((err)=>{
         res.send(err)
+        console.log(err);
     })
 }
 
-
+//empty cart
 delete2=(req,res)=>{
     knex.delete("*").from("shopping_cart").where("cart_id",req.params.id)
     .then((data)=>{
@@ -70,6 +90,31 @@ delete2=(req,res)=>{
   
   
   }
-  
+ 
+// get total amount
+getAmount=(req,res)=>{
+    knex.select("*").from("shopping_cart").join("product","shopping_cart.product_id","product.product_id").where("cart_id",req.params.id)
+    .then((data)=>{
+        total_amount=0
+        for ( i of data){
+            total_amount+=data[0].quantity*data[0].price
+        }
+        // console.log(total_amount);
+        res.send({"total_amount":total_amount})
 
-module.exports={generateId,addShopping_cart,getp,Update,delete2}
+    }).catch((err)=>{
+        res.send(err)
+    })
+}
+
+// remove product
+removeProduct=(req,res)=>{
+    knex.delete("*").from("shopping_cart").where("product_id",req.params.id)
+    .then((data)=>{
+        res.send("no data")
+    }).catch((err)=>{
+        res.send(err)
+    })
+}
+
+module.exports={generateId,addShopping_cart,getp,Update1,delete2,getAmount,removeProduct}
